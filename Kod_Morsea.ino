28db76cc2057f9f93fdb849e9c821aba6a7b8a9c
Michał Słowiński <62020662+mslowin@users.noreply.github.com>
2024-08-06 21:07:33 +0200

    Update code, add second button
    Second button to go back one letter

--- Kod_Morsea.ino
+++ Kod_Morsea.ino
@@ -2,15 +2,17 @@
 
 const int ledPin = 7;
 const int buzzerPin = 13;
-const int buttonPin = 8;
-const int longSignalTime = 500;   // Duration of a long signal in milliseconds
-const int shortSignalTime = 200;  // Duration of a short signal in milliseconds
-const int pauseTime = 100;   // Duration of a pause in milliseconds
+const int nextCharacterButtonPin = 8;
+const int repeatButtonPin = A5;
+const int previousCharacterButtonPin = A4;
+const int longSignalTime = 900;   // Duration of a long signal in milliseconds
+const int shortSignalTime = 400;  // Duration of a short signal in milliseconds
+const int pauseTime = 250;   // Duration of a pause in milliseconds
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int characterNumber = 0; 
+int characterNumber = 1; 
 
 void setup() {
   // set up the LCD's number of columns and rows:
@@ -21,62 +23,124 @@ void setup() {
   
   pinMode(buzzerPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(nextCharacterButtonPin, INPUT_PULLUP);
+  pinMode(repeatButtonPin, INPUT_PULLUP);
+  pinMode(previousCharacterButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
+  int nextCharacterButtonState = digitalRead(nextCharacterButtonPin);
+  int repeatButtonState = digitalRead(repeatButtonPin);
+  int previousCharacterButtonState = digitalRead(previousCharacterButtonPin);
 
-  if (buttonState == HIGH)
+  if (repeatButtonState == LOW)
+  {
+    // when button is pressed
+//    digitalWrite(ledPin, HIGH);
+//    delay(10000);
+//    digitalWrite(ledPin, LOW);
+    characterNumber--;
+    LcdWriteCharacterNumber(characterNumber);
+
+    delay(1000);
+  }
+  
+  if (nextCharacterButtonState == HIGH)
   {
     // when button is not pressed do nothing
     return;
   }
 
   // When button is pressed then:
-  
-  if (characterNumber == 0) {
-    // First letter: S (three short signals)
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Litera nr:");
-    lcd.setCursor(0, 1);
-    lcd.print("1");
-    shortSignal();
-    shortSignal();
-    shortSignal();
-    characterNumber++;
-  }
-  else if (characterNumber == 1) {
-    // Second letter: O (three long signals)
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Litera nr:");
-    lcd.setCursor(0, 1);
-    lcd.print("2");
-    longSignal();
-    longSignal();
-    longSignal();
+  if (characterNumber == 1) {
+    LcdWriteCharacterNumber(characterNumber);
+    E_letter();
     characterNumber++;
   }
   else if (characterNumber == 2) {
-    // third letter: S (three short signals)
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Litera nr:");
-    lcd.setCursor(0, 1);
-    lcd.print("1");
-    shortSignal();
-    shortSignal();
-    shortSignal();
+    LcdWriteCharacterNumber(characterNumber);
+    I_letter();
     characterNumber++;
   }
   else if (characterNumber == 3) {
+    LcdWriteCharacterNumber(characterNumber);
+    C_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 4) {
+    LcdWriteCharacterNumber(characterNumber);
+    M_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 5) {
+    LcdWriteCharacterNumber(characterNumber);
+    A_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 6) {
+    LcdWriteCharacterNumber(characterNumber);
+    H_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 7) {
+    LcdWriteCharacterNumber(characterNumber);
+    C_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 8) {
+    LcdWriteCharacterNumber(characterNumber);
+    O_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 9) {
+    LcdWriteCharacterNumber(characterNumber);
+    K_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 10) {
+    LcdWriteCharacterNumber(characterNumber);
+    O_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 11) {
+    LcdWriteCharacterNumber(characterNumber);
+    D_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 12) {
+    LcdWriteCharacterNumber(characterNumber);
+    T_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 13) {
+    LcdWriteCharacterNumber(characterNumber);
+    Y_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 14) {
+    LcdWriteCharacterNumber(characterNumber);
+    L_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 15) {
+    LcdWriteCharacterNumber(characterNumber);
+    U_letter();
+    characterNumber++;
+  }
+  else if (characterNumber == 16) {
     characterNumber = 0;
   }
-  
-  // Small delay to debounce the button
-  delay(50);
+
+  delay(50); // Small delay to debounce the button
+}
+
+void LcdWriteCharacterNumber(int number)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Litera nr:");
+  lcd.setCursor(0, 1);
+  lcd.print(number);
 }
 
 void longSignal()
@@ -100,4 +164,190 @@ void przerywnik()
   digitalWrite(buzzerPin, LOW);
   digitalWrite(ledPin, LOW);
   delay(pauseTime);
+}
+
+void A_letter()
+{
+  shortSignal();
+  longSignal();
+}
+
+void B_letter()
+{
+  longSignal();
+  shortSignal();
+  shortSignal();
+  shortSignal();
+}
+
+void C_letter()
+{
+  longSignal();
+  shortSignal();
+  longSignal();
+  shortSignal();
+}
+
+void D_letter()
+{
+  longSignal();
+  shortSignal();
+  shortSignal();
+}
+
+void E_letter()
+{
+  shortSignal();
+}
+
+void F_letter()
+{
+  shortSignal();
+  shortSignal();
+  longSignal();
+  shortSignal();
+}
+
+void G_letter()
+{
+  longSignal();
+  longSignal();
+  shortSignal();
+}
+
+void H_letter()
+{
+  shortSignal();
+  shortSignal();
+  shortSignal();
+  shortSignal();
+}
+
+void I_letter()
+{
+  shortSignal();
+  shortSignal();
+}
+
+void J_letter()
+{
+  shortSignal();
+  longSignal();
+  longSignal();
+  longSignal();
+}
+
+void K_letter()
+{
+  longSignal();
+  shortSignal();
+  longSignal();
+}
+
+void L_letter()
+{
+  shortSignal();
+  longSignal();
+  shortSignal();
+  shortSignal();
+}
+
+void M_letter()
+{
+  longSignal();
+  longSignal();
+}
+
+void N_letter()
+{
+  longSignal();
+  shortSignal();
+}
+
+void O_letter()
+{
+  longSignal();
+  longSignal();
+  longSignal();
+}
+
+void P_letter()
+{
+  shortSignal();
+  longSignal();
+  longSignal();
+  shortSignal();
+}
+
+void Q_letter()
+{
+  longSignal();
+  longSignal();
+  shortSignal();
+  longSignal();
+}
+
+void R_letter()
+{
+  longSignal();
+  shortSignal();
+  longSignal();
+}
+
+void S_letter()
+{
+  shortSignal();
+  shortSignal();
+  shortSignal();
+}
+
+void T_letter()
+{
+  longSignal();
+}
+
+void U_letter()
+{
+  shortSignal();
+  shortSignal();
+  longSignal();
+}
+
+void V_letter()
+{
+  shortSignal();
+  shortSignal();
+  shortSignal();
+  longSignal();
+}
+
+void W_letter()
+{
+  shortSignal();
+  longSignal();
+  longSignal();
+}
+
+void X_letter()
+{
+  longSignal();
+  shortSignal();
+  shortSignal();
+  longSignal();
+}
+
+void Y_letter()
+{
+  longSignal();
+  shortSignal();
+  longSignal();
+  longSignal();
+}
+
+void Z_letter()
+{
+  longSignal();
+  longSignal();
+  shortSignal();
+  shortSignal();
 }
